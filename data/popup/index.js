10adfff0f4e93d7bc43f0c1925c4ff66b0fd8e48
Duc Bui <ducgrand2001@gmail.com>
2025-04-09 17:28:50 +0700

    update logo

--- data/popup/index.js
+++ data/popup/index.js
@@ -1,16 +1,17 @@
-document.addEventListener('click', ({ target }) => {
-  const cmd = target.dataset.cmd;
+document.addEventListener('DOMContentLoaded', function() {
+  const openButton = document.getElementById('open-youtube-button');
+  
+  openButton.addEventListener('click', () => {
+    const ua = "Mozilla/5.0 (SMART-TV; Linux; Tizen 2.3) AppleWebkit/538.1 (KHTML, like Gecko) SamsungBrowser/1.0 TV Safari/538.1"
+    const url = 'https://www.youtube.com/tv/guest'
 
-  if (cmd && cmd === "tab") {
-
-    const value = "Mozilla/5.0 (SMART-TV; Linux; Tizen 2.3) AppleWebkit/538.1 (KHTML, like Gecko) SamsungBrowser/1.0 TV Safari/538.1"
     chrome.windows.create({
-      url: 'https://www.youtube.com/tv',
+      url,
       type: 'popup'
     }, (window) => {
       chrome.storage.session.set({
         [window.tabs[0].id]: {
-          ua: value,
+          ua,
           cookieStoreId: window.tabs[0].cookieStoreId
         }
       }, () => {
@@ -18,17 +19,14 @@ document.addEventListener('click', ({ target }) => {
           chrome.tabs.reload(window.tabs[0].id, {
             bypassCache: true
           });
-        }, 500)
+        }, 500);
       });
     });
+  });
+
+  const currentYear = new Date().getFullYear();
+  const footerYearElement = document.getElementById('footer-year');
+  if (footerYearElement) {
+    footerYearElement.textContent = currentYear;
   }
-
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    const currentYear = new Date().getFullYear();
-    const footerYearElement = document.getElementById('footer-year');
-    if (footerYearElement) {
-        footerYearElement.textContent = currentYear;
-    }
 });
